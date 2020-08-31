74ca56ec0cfd7b8e097d41f2cf6f0709c62faf13
Ken Moini <ken@kenmoini.com>
2020-08-31 01:24:24 -0400

    modify boot_init

--- hub/raspi-prep/boot_init.sh
+++ hub/raspi-prep/boot_init.sh
@@ -1,24 +1,11 @@
-# 1. MAKING THE SYSTEM WORK. DO NOT REMOVE
-mount -t tmpfs tmp /run
-mkdir -p /run/systemd
-mount / -o remount,rw
-sed -i 's| init=.*||' /boot/cmdline.txt
+#!/bin/bash
 
-# 2. THE USEFUL PART OF THE SCRIPT
-PUB_KEY=$(cat /boot/pi_public_ssh_key)
-umask 0077
-mkdir -p /home/pi/.ssh
-touch /home/pi/.ssh/authorized_keys
-grep -q -F "$PUB_KEY" /home/pi/.ssh/authorized_keys 2>/dev/null || echo "$PUB_KEY" >> /home/pi/.ssh/authorized_keys
-chown -R pi:pi /home/pi/.ssh
-
-/usr/lib/raspi-config/init_resize.sh
-
-# 3. CLEANING UP AND REBOOTING
-sync
-umount /boot
-mount / -o remount,ro
-sync
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
-sleep 5
+# Install Pi user SSH Key
+if [ -f "/boot/pi_public_ssh_key" ]; then
+  PUB_KEY=$(cat /boot/pi_public_ssh_key)
+  umask 0077
+  mkdir -p /home/pi/.ssh
+  touch /home/pi/.ssh/authorized_keys
+  grep -q -F "$PUB_KEY" /home/pi/.ssh/authorized_keys 2>/dev/null || echo "$PUB_KEY" >> /home/pi/.ssh/authorized_keys
+  chown -R pi:pi /home/pi/.ssh
+fi
